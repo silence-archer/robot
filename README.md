a93690aa805ff3c4ba87892e8e678e710bdd6604
silence-archer <1048037315@qq.com>
2019-10-06 01:09:30 +0800

    增肌H2数据库配置

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # robot
 后台管理系统服务端
+## H2连接字符串参数
+DB_CLOSE_DELAY=-1：要求最后一个正在连接的连接断开后，不要关闭数据库
+MODE=MySQL：兼容模式，H2兼容多种数据库，该值可以为：DB2、Derby、HSQLDB、MSSQLServer、MySQL、Oracle、PostgreSQL
+AUTO_RECONNECT=TRUE：连接丢失后自动重新连接
+AUTO_SERVER=TRUE：启动自动混合模式，允许开启多个连接，该参数不支持在内存中运行模式
+TRACE_LEVEL_SYSTEM_OUT、TRACE_LEVEL_FILE：输出跟踪日志到控制台或文件， 取值0为OFF，1为ERROR（默认值），2为INFO，3为DEBUG
+SET TRACE_MAX_FILE_SIZE mb：设置跟踪日志文件的大小，默认为16M
--- src/test/java/com/silence/robot/RobotApplicationTests.java
+++ src/test/java/com/silence/robot/RobotApplicationTests.java
@@ -2,15 +2,27 @@ package com.silence.robot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RobotApplicationTests {
 
+    @Autowired(required = false)
+    private DataSource dataSource;
+
     @Test
     public void contextLoads() {
+        try {
+            System.out.printf(dataSource.getConnection().getSchema());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
