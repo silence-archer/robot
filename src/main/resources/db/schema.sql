7339074c3478f006fe16b22ae6bae6656e165853
mazhaohui <mazhba@dcits.com>
2019-10-08 14:01:19 +0800

    增加数据库脚本

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,11 +1,51 @@
-create table if not exists T_USER (
-USE_ID int not null primary key auto_increment,
-USE_NAME varchar(100),
-USE_SEX varchar(1),
-USE_AGE NUMBER(3),
-USE_ID_NO VARCHAR(18),
-USE_PHONE_NUM VARCHAR(11),
-USE_EMAIL VARCHAR(100),
-CREATE_TIME DATE,
-MODIFY_TIME DATE,
-USE_STATE VARCHAR(1));
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `ID` varchar(32) NOT NULL COMMENT '物理主键',
+  `USER_NAME` varchar(64) DEFAULT NULL COMMENT '用户名',
+  `NICKNAME` varchar(64) DEFAULT NULL COMMENT '昵称',
+  `PASSWORD` varchar(60) DEFAULT NULL COMMENT '密码',
+  `ROLE_ID` varchar(32) DEFAULT NULL COMMENT '角色ID',
+  `CREATE_TIME` datetime NOT NULL COMMENT '创建时间',
+  `CREATE_USER` varchar(32) DEFAULT NULL COMMENT '创建人',
+  `UPDATE_TIME` datetime NOT NULL COMMENT '修改时间',
+  `UPDATE_USER` varchar(32) DEFAULT NULL COMMENT '修改人',
+  PRIMARY KEY (`ID`)
+) COMMENT='用户信息表';
+
+-- ----------------------------
+-- Table structure for t_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role`;
+CREATE TABLE `t_role` (
+  `ID` varchar(32) NOT NULL COMMENT '物理主键',
+  `ROLE_NO` varchar(64) DEFAULT NULL COMMENT '角色编号',
+  `ROLE_NAME` varchar(64) DEFAULT NULL COMMENT '角色名称',
+  `MENU_ID` varchar(60) DEFAULT NULL COMMENT '菜单ID',
+  `CREATE_TIME` datetime NOT NULL COMMENT '创建时间',
+  `CREATE_USER` varchar(32) DEFAULT NULL COMMENT '创建人',
+  `UPDATE_TIME` datetime NOT NULL COMMENT '修改时间',
+  `UPDATE_USER` varchar(32) DEFAULT NULL COMMENT '修改人',
+  PRIMARY KEY (`ID`)
+) COMMENT='角色信息表';
+
+-- ----------------------------
+-- Table structure for t_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `t_menu`;
+CREATE TABLE `t_menu` (
+  `ID` varchar(32) NOT NULL COMMENT '物理主键',
+  `MENU_NO` varchar(64) DEFAULT NULL COMMENT '菜单编号',
+  `MENU_NAME` varchar(64) DEFAULT NULL COMMENT '菜单名称',
+  `MENU_PATH` varchar(60) DEFAULT NULL COMMENT '菜单路径',
+  `PARENT_MENU_NO` varchar(64) DEFAULT NULL COMMENT '父菜单编号',
+  `MENU_LEVEL` int(10) DEFAULT NULL COMMENT '菜单级别',
+  `MENU_SEQ_NO` int(10) DEFAULT NULL COMMENT '菜单序号',
+  `CREATE_TIME` datetime NOT NULL COMMENT '创建时间',
+  `CREATE_USER` varchar(32) DEFAULT NULL COMMENT '创建人',
+  `UPDATE_TIME` datetime NOT NULL COMMENT '修改时间',
+  `UPDATE_USER` varchar(32) DEFAULT NULL COMMENT '修改人',
+  PRIMARY KEY (`ID`)
+) COMMENT='角色信息表';
