06c1513b2b2e1582a757d8c17033a0af7bd1c7e8
mazhaohui <mazhaohui@csii.com.cn>
2019-11-05 17:01:27 +0800

    修改打包配置

---
+++ restart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Stopping SpringBoot Application"
+pid=`ps -ef | grep robot-web-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}'`
+if [ -n "$pid" ]
+then
+   kill -9 $pid
+fi
+echo "Starting SpringBoot Application"
+java -jar robot-web/target/robot-web-0.0.1-SNAPSHOT.jar --spring.profiles.active=h2
