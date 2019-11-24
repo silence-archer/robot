51f32688ac2aba489e566a922837a512ce00048f
mazhaohui <mazhba@dcits.com>
2019-11-24 20:22:05 +0800

    增加双人即时聊天功能

--- restart.sh
+++ restart.sh
@@ -6,4 +6,4 @@ then
    kill -9 $pid
 fi
 echo "Starting SpringBoot Application"
-nohup java -jar robot-web/target/robot-web-0.0.1-SNAPSHOT.jar --spring.profiles.active=h2 >/dev/null 2>$1 $
+nohup java -jar robot-web/target/robot-web-0.0.1-SNAPSHOT.jar --spring.profiles.active=h2 >/dev/null 2>&1 &
