f70044a7c96838dfbfee83d78418306fa73dbd96
mazhaohui <mazhba@dcits.com>
2020-01-08 14:01:30 +0800

    新增微信订阅号后台服务

--- restart.sh
+++ restart.sh
@@ -6,5 +6,4 @@ then
    kill -9 $pid
 fi
 echo "Starting SpringBoot Application"
-export BUILD_ID=DONTKILLME
 nohup java -jar robot-web/target/robot-web-0.0.1-SNAPSHOT.jar --server.port=80 --spring.profiles.active=h2 --logging.path=/var/lib/jenkins/robot-logs >/dev/null 2>&1 &
