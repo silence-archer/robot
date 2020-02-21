ea4cb2f544d4e707b7f300d2390e8b517f1acb80
mazhaohui <mazhba@dcits.com>
2020-02-21 17:26:43 +0800

    新增微信订阅号后台服务

--- restart.sh
+++ restart.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+SERVER_PORT=$1
+echo "port: $1"
 echo "Stopping SpringBoot Application"
 pid=`ps -ef | grep robot-web-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}'`
-SERVER_PORT=$1
 if [ -n "$pid" ]
 then
    kill -9 $pid
