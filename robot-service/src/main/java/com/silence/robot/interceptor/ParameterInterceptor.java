5c514d69f289a1e4612e42d660602e51e75b79dc
silence-archer <1048037315@qq.com>
2020-04-09 14:03:01 +0800

    Create ParameterInterceptor.java

---
+++ robot-service/src/main/java/com/silence/robot/interceptor/ParameterInterceptor.java
@@ -0,0 +1,64 @@
+/**
+ * @title ParameterInterceptor.java
+ * @author oe_machaohui
+ * @date 2020/3/26 18:20
+ * @copyright 2020 XXX有限公司版权所有
+ */
+package com.silence.robot.interceptor;
+
+import com.silence.robot.utils.BeanUtils;
+import com.silence.robot.utils.CommonUtils;
+import com.silence.robot.utils.HttpUtils;
+import org.apache.catalina.manager.util.SessionUtils;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.executor.parameter.ParameterHandler;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlCommandType;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.scripting.defaults.DefaultParameterHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.PreparedStatement;
+import java.util.Date;
+import java.util.Properties;
+
+/**
+ * @author oe_machaohui
+ * @description mybatis 插入更新操作拦截器
+ * @date 2020/3/26 18:20
+ */
+@Intercepts({@Signature(type= Executor.class, method = "update", args = {MappedStatement.class, Object.class})})
+public class ParameterInterceptor implements Interceptor {
+
+    private final Logger logger = LoggerFactory.getLogger(ParameterInterceptor.class);
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        Executor executor = (Executor) invocation.getTarget();
+        Object[] args = invocation.getArgs();
+        MappedStatement mappedStatement = (MappedStatement) args[0];
+        if(SqlCommandType.INSERT.equals(mappedStatement.getSqlCommandType())) {
+            BeanUtils.setProperty(args[1], "id", CommonUtils.getUuid());
+            BeanUtils.setProperty(args[1], "createTime", new Date());
+            BeanUtils.setProperty(args[1], "createUser", HttpUtils.getLoginUserName());
+            BeanUtils.setProperty(args[1], "updateTime", new Date());
+            BeanUtils.setProperty(args[1], "updateUser", HttpUtils.getLoginUserName());
+        }else if(SqlCommandType.UPDATE.equals(mappedStatement.getSqlCommandType())) {
+            BeanUtils.setProperty(args[1], "updateTime", new Date());
+            BeanUtils.setProperty(args[1], "updateUser", HttpUtils.getLoginUserName());
+        }
+        Object proceed = invocation.proceed();
+        return proceed;
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
